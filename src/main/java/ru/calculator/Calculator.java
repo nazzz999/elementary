8832cf1574fcb92919ca94e19f56de6c0f4db672
nazarnass <nazarychevaanastacia@yandex.ru>
2024-10-01 17:51:08 +0300

    1.3 Арифметические операции

---
+++ src/main/java/ru/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int oneRlusTwo = one + two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println("1 + 2 = " + oneRlusTwo);
+        System.out.println("6 / 2 = " + sixDivTwo);
+        System.out.println("5 - 2 = " + fiveMinusTwo);
+        System.out.println("4 * 2 = " + fourTimeTwo);
+    }
+}
