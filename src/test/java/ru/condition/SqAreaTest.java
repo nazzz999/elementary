149eae92b140b9a09d6e3aface38c4dfbfb2a2dc
nazarnass <nazarychevaanastacia@yandex.ru>
2024-10-10 21:56:06 +0300

    2.6

---
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -0,0 +1,35 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP3K4Square0dot36() {
+        double expected = 0.36;
+        int p = 3;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP4K2Square0dot888() {
+        double expected = 0.888;
+        int p = 4;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
