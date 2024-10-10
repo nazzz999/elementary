8886933c6f182377c50d40274670721efbdb0c48
nazarnass <nazarychevaanastacia@yandex.ru>
2024-10-10 16:47:11 +0300

    2.2 Что такое import

---
+++ src/main/java/ru/calculator/MathCalculator.java
@@ -0,0 +1,26 @@
+package ru.calculator;
+
+import static ru.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double differenceAndDivision(double first, double second) {
+        return difference(first, second) + division(first, second);
+    }
+
+    public static double sumMultiplyDifferenceDivision(double first, double second) {
+        return sum(first, second) + multiply(first, second)
+                + difference(first, second) + division(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + differenceAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + sumMultiplyDifferenceDivision(10, 20));
+    }
+}
