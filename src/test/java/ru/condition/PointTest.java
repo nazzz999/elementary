8f650e25f4270c705eedd9f299cc677fb3efac7a
nazarnass <nazarychevaanastacia@yandex.ru>
2024-10-10 21:48:47 +0300

    2.5

---
+++ src/test/java/ru/condition/PointTest.java
@@ -0,0 +1,40 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when00to00then0() {
+        double expected = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when11to22then1dot41() {
+        double expected = 1.41;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
