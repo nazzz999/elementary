1d4ac1e3b7c09edcba4c3a1229d4570210b237ab
nazarnass <nazarychevaanastacia@yandex.ru>
2024-10-30 23:00:21 +0300

    5.2 Размер массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива ages: " + ages.length + " символов");
         String[] surnames = new String[100500];
+        System.out.println("Размер массива surnames: " + surnames.length + " символов");
         float[] prices = new float[40];
+        System.out.println("Размер массива prices: " + prices.length + " символов");
     }
 }
