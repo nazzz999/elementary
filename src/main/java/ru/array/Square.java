95f814e7e8eca5d942142e38877ca133d153709e
nazarnass <nazarychevaanastacia@yandex.ru>
2024-10-31 16:12:25 +0300

    5_6_Заполнить_массив_степенями_чисел

--- src/main/java/ru/array/Square.java
+++ src/main/java/ru/array/Square.java
@@ -2,11 +2,11 @@ package ru.array;
 
 public class Square {
     public static int[] calculate(int bound) {
-        int[] arrayTwo = new int[bound];
-        for (int index = 0; index < arrayTwo.length; index++) {
-            arrayTwo[index] = (int) Math.pow(index, 2);
+        int[] array = new int[bound];
+        for (int index = 0; index < bound; index++) {
+            array[index] = (int) Math.pow(index, 2);
         }
-        return arrayTwo;
+        return array;
     }
 
     public static void main(String[] args) {
