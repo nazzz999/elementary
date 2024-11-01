43c57c05fb701c8712a42353de44bdc43710681e
nazarnass <nazarychevaanastacia@yandex.ru>
2024-11-01 21:23:00 +0300

    5_15_Сравнить_последние_элементы_двух_массивов

---
+++ src/main/java/ru/array/EqLast.java
@@ -0,0 +1,12 @@
+package ru.array;
+
+public class EqLast {
+    public static boolean check(int[] left, int[] right) {
+        for (int index = 0; index < left.length; index++) {
+            if (left[left.length - 1] == right[right.length - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
