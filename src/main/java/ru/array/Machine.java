b1cc61715f7608afcafc130c5a8811688eecd85d
nazarnass <nazarychevaanastacia@yandex.ru>
2024-11-16 00:08:35 +0300

    5_32_Жадный_алгоритм_Сдача_в_кофе_машине

--- src/main/java/ru/array/Machine.java
+++ src/main/java/ru/array/Machine.java
@@ -5,17 +5,16 @@ import java.util.Arrays;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
-        int i = 0;
         int[] rsl = new int[100];
         int size = 0;
         int changes = money - price;
         while (changes != 0) {
-            if (changes >= coins[i]) {
-                changes -= coins[i];
-                rsl[size] = coins[i];
-                size++;
-            } else {
-                i++;
+            for (int coin : coins) {
+                while (changes >= coin) {
+                    changes -= coin;
+                    rsl[size] = coin;
+                    size++;
+                }
             }
         }
         return Arrays.copyOf(rsl, size);
