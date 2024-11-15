88700e27f5963d3e49c12bbf0648d6e1e413609d
nazarnass <nazarychevaanastacia@yandex.ru>
2024-11-15 03:06:26 +0300

    5.31 Метод двух указателей

--- src/main/java/ru/calculator/Out.java
+++ src/main/java/ru/calculator/Out.java
@@ -7,7 +7,7 @@ public class Out {
         Scanner scanner = new Scanner(System.in);
         int password;
         do {
-            System.out.println("Введите пароль: ");
+            System.out.println("Введите пароль:  ");
             password = scanner.nextInt();
         } while (password != 555);
         System.out.println("Пароль введён успешно!");
